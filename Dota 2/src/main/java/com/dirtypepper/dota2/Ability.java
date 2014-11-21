8543ff0779f9e6b82a3b80f78d92846badc43e98
Stefenatefun <stefenatefun@gmail.com>
2014-11-21 18:39:00 -0500

    Add setters to ability methods.

--- Dota 2/src/main/java/com/dirtypepper/dota2/Ability.java
+++ Dota 2/src/main/java/com/dirtypepper/dota2/Ability.java
@@ -5,26 +5,30 @@ import java.util.Map;
 
 public class Ability {
 
-    private final String name;
-    private final String description;
-    private final String ability;
-    private final String affects;
-    private final String damage;
-    private final Map<String, String> attributes;
-    private final boolean orbOfVenom;
-    private final int blackKingBar;
-    private final int linkensSphere;
-    private final int diffusalBlade;
-    private final int mantaStyle;
-    private final String cooldown;
-    private final String mana;
-    private final String blackKingBarDescription;
-    private final String linkensSphereDescription;
-    private final String diffusalBladeDescription;
-    private final String mantaStyleDescription;
-    private final String altDescription;
-    private final String aghanims;
-    private final ArrayList<String> notes;
+    private String name;
+    private String description;
+    private String ability;
+    private String affects;
+    private String damage;
+    private Map<String, String> attributes;
+    private boolean orbOfVenom;
+    private int blackKingBar;
+    private int linkensSphere;
+    private int diffusalBlade;
+    private int mantaStyle;
+    private String cooldown;
+    private String mana;
+    private String blackKingBarDescription;
+    private String linkensSphereDescription;
+    private String diffusalBladeDescription;
+    private String mantaStyleDescription;
+    private String altDescription;
+    private String aghanims;
+    private ArrayList<String> notes;
+
+    public Ability() {
+
+    }
 
     public Ability(String name, String description, String ability, String affects, String damage, Map<String, String> attributes, boolean orbOfVenom, int blackKingBar, int linkensSphere, int diffusalBlade, int mantaStyle, String cooldown, String mana, String blackKingBarDescription, String diffusalBladeDescription, String linkensSphereDescription, String mantaStyleDescription, String altDescription, String aghanims, ArrayList<String> notes) {
         this.name = name;
@@ -57,80 +61,160 @@ public class Ability {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getAbility() {
         return ability;
     }
 
+    public void setAbility(String ability) {
+        this.ability = ability;
+    }
+
     public String getAffects() {
         return affects;
+    }
+
+    public void setAffects(String affects) {
+        this.affects = affects;
     }
 
     public String getDamage() {
         return damage;
     }
 
+    public void setDamage(String damage) {
+        this.damage = damage;
+    }
+
     public Map<String, String> getAttributes() {
         return attributes;
+    }
+
+    public void setAttributes(Map<String, String> attributes) {
+        this.attributes = attributes;
     }
 
     public boolean getOrbOfVenom() {
         return orbOfVenom;
     }
 
+    public void setOrbOfVenom(boolean orbOfVenom) {
+        this.orbOfVenom = orbOfVenom;
+    }
+
     public int getBlackKingBar() {
         return blackKingBar;
+    }
+
+    public void setBlackKingBar(int blackKingBar) {
+        this.blackKingBar = blackKingBar;
     }
 
     public int getLinkensSphere() {
         return linkensSphere;
     }
 
+    public void setLinkensSphere(int linkensSphere) {
+        this.linkensSphere = linkensSphere;
+    }
+
     public int getDiffusalBlade() {
         return diffusalBlade;
+    }
+
+    public void setDiffusalBlade(int diffusalBlade) {
+        this.diffusalBlade = diffusalBlade;
     }
 
     public int getMantaStyle() {
         return mantaStyle;
     }
 
+    public void setMantaStyle(int mantaStyle) {
+        this.mantaStyle = mantaStyle;
+    }
+
     public String getCooldown() {
         return cooldown;
+    }
+
+    public void setCooldown(String cooldown) {
+        this.cooldown = cooldown;
     }
 
     public String getMana() {
         return mana;
     }
 
+    public void setMana(String mana) {
+        this.mana = mana;
+    }
+
     public String getBlackKingBarDescription() {
         return blackKingBarDescription;
+    }
+
+    public void setBlackKingBarDescription(String blackKingBarDescription) {
+        this.blackKingBarDescription = blackKingBarDescription;
     }
 
     public String getLinkensSphereDescription() {
         return linkensSphereDescription;
     }
 
+    public void setLinkensSphereDescription(String linkensSphereDescription) {
+        this.linkensSphereDescription = linkensSphereDescription;
+    }
+
     public String getDiffusalBladeDescription() {
         return diffusalBladeDescription;
+    }
+
+    public void setDiffusalBladeDescription(String diffusalBladeDescription) {
+        this.diffusalBladeDescription = diffusalBladeDescription;
     }
 
     public String getMantaStyleDescription() {
         return mantaStyleDescription;
     }
 
+    public void setMantaStyleDescription(String mantaStyleDescription) {
+        this.mantaStyleDescription = mantaStyleDescription;
+    }
+
     public String getAltDescription() {
         return altDescription;
+    }
+
+    public void setAltDescription(String altDescription) {
+        this.altDescription = altDescription;
     }
 
     public String getAghanims() {
         return aghanims;
     }
 
+    public void setAghanims(String aghanims) {
+        this.aghanims = aghanims;
+    }
+
     public ArrayList<String> getNotes() {
         return notes;
+    }
+
+    public void setNotes(ArrayList<String> notes) {
+        this.notes = notes;
     }
 
     public String toString() {
